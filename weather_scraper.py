30323ce38338f994e01c4769b726be7051cd24df
Robert Young <robert.young.ie@gmail.com>
2019-02-21 20:09:01 +0000

    Weather Scraper Complete
    Weather scraper now working correctly for current weather. Next step is EC2 deployment

--- weather_scraper.py
+++ weather_scraper.py
@@ -2,57 +2,91 @@ import json
 import pandas as pd
 import numpy as numpy
 import requests
+import sqlalchemy as sqla
+from sqlalchemy import create_engine
+import traceback
 import os
+from IPython.display import display
 import datetime
 
 # API weather URI for city ID "Dublin, IE"
 api_url_base_weather = ***REMOVED***
 api_token_weather = ***REMOVED***
 
-# Pull json data with api token provided above
-r = requests.get(api_url_base_weather, params={"APPID": api_token_weather})
+# Database access details
+URI = ***REMOVED***
+DB = ***REMOVED***
+PORT=***REMOVED***
+USER=***REMOVED***
+PASSWORD=***REMOVED***
 
-# Decode json to a text format
-data_weather = json.JSONDecoder().decode(r.text)
+engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
-# JSON data is nested, normalize it
-df = pd.io.json.json_normalize(data_weather)
+def weather_to_db(df):
+    try:
+        df.to_sql('current_weather', con=engine,if_exists='append', index=False)
+    except:
+        f= open("logTracebackError.log","a+")
+        print(traceback.format_exc())
+        f.write(traceback.format_exc())
+        f.close()
+        print("operation complete")
+    return
 
-# take a data-time stamp to add to the dataframe
-currentDT = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+try:
 
-df['last_update'] = str(currentDT)
+    # Pull json data with api token provided above
+    r = requests.get(api_url_base_weather, params={"APPID": api_token_weather})
 
-# Split out the weather data array into individual new columns
-df['description'] = df['weather'][0][0]['description']
-df['main_weather'] = df['weather'][0][0]['main']
-df['icon'] = df['weather'][0][0]['icon']
-df['weather_id'] = df['weather'][0][0]['id']
+    # Decode json to a text format
+    data_weather = json.JSONDecoder().decode(r.text)
 
-# Drop the weather column and base column https://openweathermap.org/weather-data
-df = df.drop(['weather', 'base', 'coord.lat', 'coord.lon', 'sys.country', 'sys.message', 'sys.id', 'sys.type', 'name']
-             , axis=1)
+    # JSON data is nested, normalize it
+    df = pd.io.json.json_normalize(data_weather)
 
-# Check if csv exists, if not, create one
-csv_exists = os.path.isfile('data_backup_weather.csv')
-if not csv_exists:
-    print("csv doesn't exist")
-    df.to_csv('data_backup_weather.csv',index=False)
-else:
-    # Append data to csv file
-    print("csv does exist")
-    with open('data_backup_weather.csv', 'a') as f:
-        df.to_csv(f, header=False,index=False)
-    
-# Check if text file exists, if not, create one
-text_exists = os.path.isfile('data_weather.txt')
-if not text_exists:
-    print("txt doesn't exist")
-    with open('data_weather.txt', 'w') as outfile:  
-        json.dump(data_weather, outfile)
-else:
-    # Append data to txt
-    print("txt does exist")
-    with open('data_weather.txt', 'a') as outfile:
-        json.dump(data_weather, outfile)
+    # take a date-time stamp to add to the dataframe
+    currentDT = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+    # Add the time stamp to the dataframe
+    df['last_update'] = str(currentDT)
+
+    # Split out the weather data array into individual new columns
+    df['description'] = df['weather'][0][0]['description']
+    df['main_weather'] = df['weather'][0][0]['main']
+    df['icon'] = df['weather'][0][0]['icon']
+    df['weather_id'] = df['weather'][0][0]['id']
+
+    # Dropped internal system and static data https://openweathermap.org/weather-data
+    df = df.drop(['weather', 'base', 'coord.lat', 'coord.lon', 'sys.country', 'sys.message', 'sys.id', 'sys.type', 'name']
+                 , axis=1)
+
+    # Send the dataframe to the RDS database table "current_weather"
+    weather_to_db(df)
+
+    # Check if csv exists, if not, create one
+    csv_exists = os.path.isfile('data_backup_weather.csv')
+    if not csv_exists:
+        print("csv doesn't exist")
+        df.to_csv('data_backup_weather.csv',index=False)
+    else:
+        # Append data to csv file
+        print("csv does exist")
+        with open('data_backup_weather.csv', 'a') as f:
+            df.to_csv(f, header=False,index=False)
+
+    # Check if text file exists, if not, create one
+    text_exists = os.path.isfile('data_weather.txt')
+    if not text_exists:
+        print("txt doesn't exist")
+        with open('data_weather.txt', 'w') as outfile:
+            json.dump(data_weather, outfile)
+    else:
+        # Append data to txt
+        print("txt does exist")
+        with open('data_weather.txt', 'a') as outfile:
+            json.dump(data_weather, outfile)
+except:
+    f= open("logTracebackError.log","a+")
+    print(traceback.format_exc())
+    f.write(traceback.format_exc())
+    f.close()
