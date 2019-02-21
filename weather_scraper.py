4929859bebddd413432b50710d0d83e6046f6a7b
Robert Young <robert.young.ie@gmail.com>
2019-02-21 16:35:24 +0000

    Date time and comments
    Date-time stamp added to the dataframe, additional comments

--- weather_scraper.py
+++ weather_scraper.py
@@ -1,17 +1,27 @@
-
 import json
 import pandas as pd
 import numpy as numpy
 import requests
 import os
+import datetime
 
+# API weather URI for city ID "Dublin, IE"
 api_url_base_weather = ***REMOVED***
 api_token_weather = ***REMOVED***
+
+# Pull json data with api token provided above
 r = requests.get(api_url_base_weather, params={"APPID": api_token_weather})
 
+# Decode json to a text format
 data_weather = json.JSONDecoder().decode(r.text)
 
+# JSON data is nested, normalize it
 df = pd.io.json.json_normalize(data_weather)
+
+# take a data-time stamp to add to the dataframe
+currentDT = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+df['last_update'] = str(currentDT)
 
 # Split out the weather data array into individual new columns
 df['description'] = df['weather'][0][0]['description']
