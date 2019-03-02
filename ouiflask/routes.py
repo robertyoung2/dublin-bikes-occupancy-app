695339499d6208de14cd3bf827ac7fd8331739b4
Conor Lonergan <conorlonergan91@gmail.com>
2019-03-02 13:00:01 +0000

    Descriptive comments added with some notes on how to modify and improve current code

--- ouiflask/routes.py
+++ ouiflask/routes.py
@@ -13,10 +13,19 @@ DB = ***REMOVED***
 engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
 connection = engine.connect() #connects the engine to the database
-result = connection.execute("select address, position_lat, position_lng from stations") #stores results from db query
 
-#Adapted from https://www.geeksforgeeks.org/python-convert-list-tuples-dictionary/
-#Function to convert SQLAlchemy ResultProxy execute(query) return type into python list = [[add, lat, lng],[...]]
+"""
+SQL query to get data from RDS db stations table
+To populate more details on the InfoWindow, add them to query here
+"""
+result = connection.execute("select address, position_lat, position_lng from stations")
+
+"""
+Adapted from https://www.geeksforgeeks.org/python-convert-list-tuples-dictionary/
+Function to convert SQLAlchemy ResultProxy execute(query) return type into python list = [[add, lat, lng],[...]]
+If details added to the query, make sure to account for them in this function also
+This function could auto adjust for extra variables added to query using nested for loop. Implement later
+"""
 def Convert(myTuple, myList):
     for add, lat, lng in myTuple:
         myList.append([add, lat, lng])
@@ -28,7 +37,7 @@ myList = []
 result = Convert(myTuple, myList)
 
 
-# JSON_result = json.dumps(result) #convert list into JSON format (removed for the minute)
+# JSON_result = json.dumps(result) #convert list into JSON format (removed atm as having issues with formatting)
 
 connection.close() # Close engine connection to tidy up resources
 
@@ -36,5 +45,5 @@ connection.close() # Close engine connection to tidy up resources
 @app.route("/")
 @app.route("/home")
 def home():
-    # Passing the dictionary result into the home.html page as variable results
+    # Passing the list "result" into the home.html page as variable "results"
     return render_template('home.html', results=result)
