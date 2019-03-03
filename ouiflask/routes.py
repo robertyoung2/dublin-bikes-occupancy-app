6e9bc2203d87e6702d58755c59a1e9498c2af8d4
Arnaud <bellqnger60@gmail.com>
2019-03-03 23:37:43 +0000

    The drop-down menu return now the id of the station to the back-end of the app.

--- ouiflask/routes.py
+++ ouiflask/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, flash
 from ouiflask import app
 from sqlalchemy import create_engine
 # import json #Not using yet
@@ -18,7 +18,7 @@ connection = engine.connect() #connects the engine to the database
 SQL query to get data from RDS db stations table
 To populate more details on the InfoWindow, add them to query here
 """
-result = connection.execute("select address, position_lat, position_lng from stations")
+result = connection.execute("select address, position_lat, position_lng, number from stations")
 
 """
 Adapted from https://www.geeksforgeeks.org/python-convert-list-tuples-dictionary/
@@ -27,8 +27,8 @@ If details added to the query, make sure to account for them in this function al
 This function could auto adjust for extra variables added to query using nested for loop. Implement later
 """
 def Convert(myTuple, myList):
-    for add, lat, lng in myTuple:
-        myList.append([add, lat, lng])
+    for add, lat, lng, number in myTuple:
+        myList.append([add, lat, lng, number])
     return myList
 
 
@@ -42,8 +42,22 @@ result = Convert(myTuple, myList)
 connection.close() # Close engine connection to tidy up resources
 
 #Allows access to home.html through the browser by typing either /home or nothing at the end of url
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["GET","POST"])
+@app.route("/home", methods=["GET","POST"])
 def home():
+    test = "1er"
+    try:
+        if request.method == "POST":
+            test = "posted"
+            flash("posted something")
+            stationID = request.form['stationID']
+            return render_template('home.html', results=result, test=test, stationIDposted=stationID)
+        return render_template('home.html', results=result, test=test)
+
+
+
+    except Exception as e:
+        test = "excepted"
+        flash(e)
     # Passing the list "result" into the home.html page as variable "results"
-    return render_template('home.html', results=result)
+        return render_template('home.html', results=result, test=test)
