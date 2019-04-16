f5cdb4fd3360c694fe0a0205478840fc18c6e699
Conor Lonergan <conorlonergan91@gmail.com>
2019-04-16 09:43:09 +0100

    File Path for EC2 Merge
    Adjusted pickle file path for EC2 Merge

--- ouiflask/getPredictions.py
+++ ouiflask/getPredictions.py
@@ -43,7 +43,7 @@ def predict(station_id, maxBikes, name):
     for day in days:
         hourly = []
         predictedDays.append(day[1])
-        with open(***REMOVED*** + 'model_' + str(station_id) + '_' + day[1] + '.pkl', 'rb') as handle:
+        with open("/home/ubuntu/oui-team/pickle_files/" + 'model_' + str(station_id) + '_' + day[1] + '.pkl', 'rb') as handle:
             rob_model = pickle.load(handle)
 
         for pred_time in range(24):
